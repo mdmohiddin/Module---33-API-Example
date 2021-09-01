bbb68da2a2e89f04419c478b18ae016b75df3171
mdmohiddin <mdmohiddin8000@gamil.com>
2021-09-01 22:19:19 +0600

    added

--- js/countries.js
+++ js/countries.js
@@ -5,15 +5,11 @@ const loadCountries = () => {
 }
 loadCountries()
 
-// দেখানোর জন্য একটা function, arrow function নিম্নে লিখি
+
 const displayCountries = countries => {
       console.log(countries);
 
-//       for (const country of countries) {
-// // countries থেকে এক একটা করে country দেখার জন্য
-//             console.log(country);
-//       }
-// অন্যভাবে, return না লাগলে map এর কাছাকাছি forEach ব্যবহার করি। অর্থাৎ যত countries আছে তার মধ্যে একটা একটা করে country দেখাবে। forEach, array'র উপরে প্রয়োগ করে।
+
       
 const countriesDiv = document.getElementById('countries');
 
@@ -22,19 +18,33 @@ const countriesDiv = document.getElementById('countries');
 
             const div = document.createElement('div');
             div.classList.add('country')
-// প্রত্যেকটা country'র জন্য একটা করে <h3> তৈরি করার জন্য
-            const h3 = document.createElement('h3');
-            h3.innerText = country.name;
-// দেখানোর জন্য countries div এর মধ্যে একটা করে <h3> চাইল্ড তৈরি করার জন্য।
-            div.appendChild(h3);
-// এক একটা country, Capital দেখার জন্য
-            const p = document.createElement('p');
+            div.innerHTML = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+            <button>Show Details</button>
+            `;
 
-            p.innerText = country.capital;
-            
-            div.appendChild(p);
+            // const h3 = document.createElement('h3');
+            // h3.innerText = country.name;
+            // div.appendChild(h3);
+            // const p = document.createElement('p');
+            // p.innerText = country.capital;
+            // div.appendChild(p);
 
             countriesDiv.appendChild(div);
-
       })
 }
+
+// দেখানোর জন্য একটা function, arrow function নিম্নে লিখি
+
+//       for (const country of countries) {
+// // countries থেকে এক একটা করে country দেখার জন্য
+//             console.log(country);
+//       }
+// অন্যভাবে, return না লাগলে map এর কাছাকাছি forEach ব্যবহার করি। অর্থাৎ যত countries আছে তার মধ্যে একটা একটা করে country দেখাবে। forEach, array'র উপরে প্রয়োগ করে।
+
+// প্রত্যেকটা country'র জন্য একটা করে <h3> তৈরি করার জন্য
+
+// দেখানোর জন্য countries div এর মধ্যে একটা করে <h3> চাইল্ড তৈরি করার জন্য।
+
+// এক একটা country, Capital দেখার জন্য
