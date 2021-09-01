8b36d2476f419b576ce627940fa48290a04a1b99
mdmohiddin <mdmohiddin8000@gamil.com>
2021-09-01 19:37:41 +0600

    commited

--- js/buddy.js
+++ js/buddy.js
@@ -7,9 +7,16 @@ loadBuddies()
 
 const displayBuddies = data => {
       const buddies = data.results;
-      // ১ data'র(৫জন buddy আছে) ভিতরে buddies গুলো পাওয়ার জন্য
-      // console.log(data.results);
+      // ১. data'র(৫জন buddy আছে) ভিতরে buddies গুলো পাওয়ার জন্য
+      console.log(data.results);
+      const buddiesDiv = document.getElementById('buddies')
       for (const buddy of buddies) {
-            console.log(buddy)
+            console.log(buddy.name.first, buddy.name.last)
+      // ২. email দেখার জন্য
+            const p = document.createElement('p');
+            // p.innerText = buddy.email;
+      // dynamic ভাবে দেখানো জন্য
+            p.innerText = `Name: ${buddy.name.title} ${buddy.name.first} Last Name: ${buddy.name.last} Email: ${buddy.email}`;
+            buddiesDiv.appendChild(p);
       }
 }
